3a92a9ed1ecc577b3b0b47dafc364e35b4596d21
Matt Verghese <matt.verghese@gmail.com>
2016-02-06 10:34:52 -0800

    Initial Commit

---
+++ HW1CodePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW1CodePath
+//
+//  Created by Matthew Verghese on 2/6/16.
+//  Copyright (c) 2016 Cheeeese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
